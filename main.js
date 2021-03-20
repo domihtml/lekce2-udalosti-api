e996be0f67a183a876f6823033fdb4efc797e1e6
domihtml <dvorak.dominika@gmail.com>
2021-03-20 20:46:34 +0100

    Add Paragraf mouseover bold function

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-// tady je místo pro náš program
+
+
+let nadpis = document.querySelector("h1");
+nadpis.classList.add("zluty");
+
+
+let odstavec = document.getElementsByClassName ("odstavec");
+
+function Ztucni(udalost){
+udalost.target.style.fontWeight = "bold";
+}
+
+function Vratzpet(udalost) {
+udalost.target.style.fontWeight ="normal";
+}
+
+/* 
+function Tucni() {
+    document.querySelector(".odstavecdva").style.fontWeight = "bold";
+}
+
+
+function Vracej() {
+    document.querySelector(".odstavecdva").style.fontWeight = "normal";
+}
+*/
